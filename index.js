040166cb9aafeb17bf85da05a8544e190558b8f3
Somesh Koli <somesh.koli@headout.com>
2022-05-10 17:58:15 +0530

    change to noasync
    Signed-off-by: Somesh Koli <somesh.koli@headout.com>

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const core = require("@actions/core")
-await core.summary
-  .addHeading('Test Results')
-  .addCodeBlock(generateTestResults(), "js")
-  .addTable([
-   [{data: "File", header: true}, {data: "Result", header: true}],
-   ["foo.js", "Pass `✅` "],
-   ["bar.js", "Fail `❌`"],
-   ["test.js", "Pass `✅`"]
-  ])
-  .addLink("View staging deployment!", "https://github.com")
-  .write()
+
+async function PublishSummary() {
+    await core.summary
+    .addHeading('Test Results')
+    .addCodeBlock("awdwadad", "js")
+    .addTable([
+        [{data: "File", header: true}, {data: "Result", header: true}],
+        ["foo.js", "Pass `✅` "],
+        ["bar.js", "Fail `❌`"],
+        ["test.js", "Pass `✅`"]
+    ])
+    .addLink("View staging deployment!", "https://github.com")
+    .write()
+}
+
+PublishSummary()
