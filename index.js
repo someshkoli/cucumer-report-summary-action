3af3d3a311b0127ce350044b04ec8cce63b81404
Somesh Koli <somesh.koli@headout.com>
2022-05-10 18:39:42 +0530

    move to json source
    Signed-off-by: Somesh Koli <somesh.koli@headout.com>

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const core = require("@actions/core")
-
+const data = require("./Cucumber.json")
 async function PublishSummary() {
+    let table = data[0].elements.map((element) => {
+        steps_result = element.steps.map((step) => [element.name, step.name, step.result.status])
+        return steps_result
+    })
+    console.log(table.flat())
     await core.summary
     .addHeading('Test Results')
     .addCodeBlock("awdwadad", "js")
     .addTable([
-        [{data: "File", header: true}, {data: "Result", header: true}],
-        ["foo.js", "Pass `✅` "],
-        ["bar.js", "Fail `❌`"],
-        ["test.js", "Pass `✅`"]
+        [{data: "Test Name", header: true}, {data: "Step Name", header: true}, {data: "Result", header: true}],
+        ...table.flat()
     ])
-    .addLink("View staging deployment!", "https://github.com")
     .write()
 }
 
